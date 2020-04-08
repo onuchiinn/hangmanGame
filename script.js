fe4186c6be99315a2f119191237ba94369a43af1
Artem Onuchin <art5654@yandex.ru>
2020-04-08 20:48:46 +0300

    Thrash... I tried to add function.....

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 
 'use strict';
 
-
 let words = [
-
     "программа",
     "макака",
     "прекрасный",
@@ -13,42 +11,71 @@ let words = [
     "грибок",
     "мануфактура"
 ];
-let word = words[Math.floor(Math.random() * words.length)];
+
+let pickWord = function (word) {
+    return word[Math.floor(Math.random() * word.length)];
+};
 
 
-let answerArray = [];
-for (let i = 0; i < word.length; i++) {
-    answerArray[i] = "_";
-}
-let remainingLetters = word.length;
-let attemp = 10;
+let setupAnswerArray = function (word) {
+    for (let i = 0; i < word.length; i++) {
+        word[i] = "_";
+    }
+};
 
 
-while ((remainingLetters > 0) && (attemp > 0)) {
-    alert(answerArray.join(" "));
+let showPlayerProgress = function (answerArray) {
+    alert(setupAnswerArray(pickWord)).join(" ");
+};
+
+let getGuess = function () {
     let guess = prompt("Угадай букву или нажмите Отмена для выхода из игры.");
     guess = guess.toLowerCase();
+}
+
+let updateGameState = function (guess, word, answerArray) {
+    guess.toLowerCase();
+    attemp--;
+    for (let j = 0; j < word.length; j++) {
+        if ((word[j] === guess) && (answerArray[j] === "_")) {
+            answerArray[j] = guess;
+            remainingLetters--;
+        }
+    }
+}
+
+let showAnswerAndCongratulatePlayer = function (answerArray) {
+    if (attemp === 0) {
+        alert("Увы. У вас закончились попытки.");
+    }
+    else {
+        alert(answerArray.join(" "));
+        alert("Отлично! Было загадано слово " + word);
+    }
+};
+
+
+
+
+
+
+
+
+
+pickWord(words);
+setupAnswerArray(pickWord);
+let attemp = 10;
+let remainingLetters = pickWord.length;
+while ((remainingLetters > 0) && (attemp > 0)) {
+
+    let guess = getGuess();
     if (guess === null) {
         break;
     } else if (guess.length !== 1) {
         alert("Пожалуйста, введите только одну букву.");
     } else {
-        guess.toLowerCase();
-        attemp--;
-        for (var j = 0; j < word.length; j++) {
-         if ((word[j] === guess) && (answerArray[j] === "_")) 
-         {
-                answerArray[j] = guess;
-                remainingLetters--;
-         }
-        }
+        let correctGuesses = updateGameState(guess, word, answerArray);
+        remainingLetters -= correctGuesses;
     }
 }
-
-if (attemp === 0) {
-    alert("Увы. У вас закончились попытки.");
-}
-else {
-    alert(answerArray.join(" "));
-    alert("Отлично! Было загадано слово " + word);
-}
+showAnswerAndCongratulatePlayer(answerArray);
